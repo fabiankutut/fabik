3deb5df6d369238a0bf505dd3cfa60c73b46e6f1
fabiyan <fabian9206@gmail.com>
2023-11-28 22:17:27 +0300

    ffff

--- fabiyan_rest/views.py
+++ fabiyan_rest/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Profile
-from .serializers import ProfileSerializers
+from .serializers import *
 
 
 @api_view(['GET'])
 def get_page2(request):
     profile = Profile.objects.all()
-    serializer = ProfileSerializers(profile, many=True)
+    serializer = ProfileRestSerializers(profile, many=True)
 
     return Response(serializer.data)
 
